6923befb0039a0e6efc33d357d572b4d8cd98f5f
sarah wolfson <swolfson23@gmail.com>
2020-01-09 16:17:13 +0100

    timer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,12 +3,15 @@ import React from 'react'
 import { NextButton } from './NextButton'
 import { Counter } from './Counter'
 
+import Timer from './timer'
+
 
 export const Footer = () => {
   return (
     <div className="container">
       <NextButton />
       <Counter />
+      <Timer />
     </div>
 
   )
