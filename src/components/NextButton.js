dc11cdb9c151bebcdf94199c6699e710d45488c7
nasimmhn <nasim.mahzoun@gmail.com>
2020-01-10 11:28:33 +0100

    submit button

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -2,14 +2,19 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { quiz } from 'reducers/quiz'
+import { useSelector } from 'react-redux'
+
+
 
 export const NextButton = () => {
+  const questions = useSelector((state) => state.quiz.questions)
+  const currentQuestionIndex = useSelector((state) => state.quiz.currentQuesionIndex)
   const dispatch = useDispatch()
 
   return (
     <>
-      <button className="nextBtn" type="button" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
-        Next
+      <button className="nextBtn" type="button" value="Next" onClick={() => dispatch(quiz.actions.goToNextQuestion())}>
+        {currentQuestionIndex === questions.length - 1 ? "Submit" : "Next"}
       </button>
     </>
   )
